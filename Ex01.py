34c5dd093aed5a4a80115867c2581e0e01718840
Gustavo <barbosagustavo54@gmail.com>
2026-06-08 07:24:47 -0300

    Primeiro Commit

---
+++ Ex01.py
@@ -0,0 +1,3 @@
+primeiro_valor = int(input("Digite um valor: "))
+segundo_valor = int(input("Digite outro valor: "))
+print(f"A soma de {primeiro_valor} e {segundo_valor} é igual a {primeiro_valor + segundo_valor}")
