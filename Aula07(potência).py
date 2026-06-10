eeac8d30201f0d3d4430eca092210172b323b3b3
Gustavo <barbosagustavo54@gmail.com>
2026-06-09 22:16:16 -0300

    Renomeando e testando o que foi feito

---
+++ Aula07(potência).py
@@ -0,0 +1,3 @@
+primeiro_numero = int(input("Digite o número: "))
+segundo_numero = int(input("Digite o valor da potência que será elevado: "))
+print (f"A potência de {primeiro_numero} elevado a {segundo_numero} é {primeiro_numero ** segundo_numero}")
