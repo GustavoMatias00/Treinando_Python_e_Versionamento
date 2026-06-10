5e1ca3105c96e3f7f9ba938a85788daa9373a348
Gustavo <barbosagustavo54@gmail.com>
2026-06-09 22:37:44 -0300

    Acrescentando subtração e divisão

---
+++ Aula07(subtração e divsão).py
@@ -0,0 +1,4 @@
+primeiro_numero = input("Digite um número: ")
+segundo_numero = input("Digite outro número: ")
+print(f"A subtração de {primeiro_numero} por {segundo_numero} é {int(primeiro_numero) - int(segundo_numero)}")
+print(f"A divisão de {primeiro_numero} por {segundo_numero} é {int(primeiro_numero) / int(segundo_numero)}")
